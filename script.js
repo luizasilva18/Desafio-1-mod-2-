f0cd2b25a465a5a6aaa8bcfeb272d6fd4f48c53e
luizasilva18clear <luizagoncalves7@gmail.com>
2021-05-12 18:17:08 -0300

    Desafio do módulo 2

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = "Luiza"
+let idade = 20
+let ano = 2021
+let atual = ano-idade
+
+console.log ("Meu ano de nascimento é", atual)
+
+let souhumano = true
+
+let number = 10
+let numero = 5
+
+console.log (number + numero)
+
